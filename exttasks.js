c2ba3f78a888d8d50a315c08f89cffeb4d5e9fc9
codenation <codenation@codenations-MacBook-Pro.local>
2019-07-25 16:55:30 +0100

    update notes

--- exttasks.js
+++ exttasks.js
@@ -1,5 +1,19 @@
-const person = {
+/* const person = {
     name: "Toqeir",
     age: 21
 };
-console.log(person);
+console.log(person); */
+
+// alarm input weekend or weekday
+var day = "Monday";
+var alarm = 7;
+
+const myAlarm = {
+    weekendAlarm: "No alarm needed",
+    weekdayAlarm: "Get up at 7am",
+};
+if (day == "Saturday" || "Sunday") {
+    console.log (myAlarm.weekendAlarm);
+} else if (day != "Saturday" || "Sunday") {
+    console.log(myAlarm.weekdayAlarm);
+}
